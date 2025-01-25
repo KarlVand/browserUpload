5e3bba083629e21bd05fc370aa842d4a5a531575
KarlVand <cg.vandooren.karl@gmail.com>
2025-01-25 14:02:19 +0100

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginForm from './elements/loginForm';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Here we start</h1>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
