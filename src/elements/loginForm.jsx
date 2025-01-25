48424fd214ccfddc0c7880db7950bb6f30e7956d
KarlVand <cg.vandooren.karl@gmail.com>
2025-01-25 14:22:36 +0100

    basic form set

--- src/elements/loginForm.jsx
+++ src/elements/loginForm.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function LoginForm() {
+const LoginForm = () => {
+    useEffect(() => {
+        document.title = "theName Login";
+    }, []);
+
+    const[input, setInput] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleSubmitEvent = (e) => {
+        e.preventDefault();
+        if (input.username !== "" && input.password !== "") {
+                  }
+        alert("please provide a valid input");
+      };
+    
+      const handleInput = (e) => {
+        const { name, value } = e.target;
+        setInput((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+      };
+
   return (
-    <form>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button type="submit">Login</button>
+    <form onSubmit={handleSubmitEvent}>
+        <label for = "username">Username</label>
+        <input type="text" placeholder="Username" onChange={handleInput} />
+        <label for = "password">Password</label>
+        <input type="password" placeholder="Password" onChange={handleInput}/>
+        <button type="submit">Login</button>
     </form>
   );
 }
